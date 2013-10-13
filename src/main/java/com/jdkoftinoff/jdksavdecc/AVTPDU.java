bd2063ffb8f416cbceb0ccb486dec577cda0b1b4
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-10-12 19:49:17 -0700

    More fields added to AVTPDU

--- src/main/java/com/jdkoftinoff/jdksavdecc/AVTPDU.java
+++ src/main/java/com/jdkoftinoff/jdksavdecc/AVTPDU.java
@@ -6,100 +6,182 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AVTPDU extends PDU {
-	
-	AVTPDU() {
-		super();
-	}
 
-	AVTPDU( PDU other ) {
-		super( other );
-	}
-	
-	AVTPDU( ByteBuffer data, byte destination_address[], byte source_address[], short ethertype ) {
-		super(data,destination_address,source_address,ethertype);
-	}
+    AVTPDU() {
+        super();
+    }
 
-	public final static byte cd_mask = (byte)0x80;
-	public final static byte CD_CONTROL = (byte)0x80;
-	public final static byte CD_DATA = (byte)0x00;
+    AVTPDU(PDU other) {
+        super(other);
+    }
 
-	public final static void setCDControl( ByteBuffer data ) {
-		setBit(data,0,0,true);
-	}
-	
-	public void setCDControl() {
-		setCDControl(data);
-	}
+    AVTPDU(ByteBuffer data, byte destination_address[], byte source_address[], short ethertype) {
+        super(data, destination_address, source_address, ethertype);
+    }
 
-	public final static boolean getCDControl( ByteBuffer data ) {
-		return getBit(data,0,0);
-	}
-	
-	public boolean getCDControl() {
-		return getCDControl(data);
-	}
-	
-	public final static byte subtype_mask = (byte)0x7f;
-	
-	public final static byte SUBTYPE_61883_IIDC = (byte)0x00;
-	public final static byte SUBTYPE_MMA = (byte)0x01;
-	public final static byte SUBTYPE_AVTP_AUDIO = (byte)0x02;
-	public final static byte SUBTYPE_AVTP_VIDEO = (byte)0x03;
-	public final static byte SUBTYPE_AVTP_CONTROL = (byte)0x04;
-	public final static byte SUBTYPE_VENDOR = (byte)0x6f;
-	public final static byte SUBTYPE_ADP = (byte)0x7a;
-	public final static byte SUBTYPE_AECP = (byte)0x7b;
-	public final static byte SUBTYPE_ACMP = (byte)0x7c;
-	public final static byte SUBTYPE_MAAP = (byte)0x7e;
-	public final static byte SUBTYPE_EXPERIMENTAL = (byte)0x7f;
-	
-	private static final Map<Integer, String> subtypeMap = createSubTypeMap();
-	
-	public static final String getSubtypeName( byte subtype ) {
-		if( subtypeMap.containsKey((int)subtype) ) {
-			return subtypeMap.get((int)subtype);
-		}
-		else {
-			return null;
-		}
-	}
+    public final static byte cd_mask = (byte) 0x80;
+    public final static byte CD_CONTROL = (byte) 0x80;
+    public final static byte CD_DATA = (byte) 0x00;
+
+    public final static void setCDControl(
+            ByteBuffer data,
+            boolean newValue) {
+        setBit(data, 0, 0, newValue);
+    }
+
+    public void setCDControl(boolean newValue) {
+        setCDControl(data, newValue);
+    }
+
+    public final static boolean getCDControl(ByteBuffer data) {
+        return getBit(data, 0, 0);
+    }
+
+    public boolean getCDControl() {
+        return getCDControl(data);
+    }
+
+    public final static byte subtype_mask = (byte) 0x7f;
+
+    public final static byte SUBTYPE_61883_IIDC = (byte) 0x00;
+    public final static byte SUBTYPE_MMA = (byte) 0x01;
+    public final static byte SUBTYPE_AVTP_AUDIO = (byte) 0x02;
+    public final static byte SUBTYPE_AVTP_VIDEO = (byte) 0x03;
+    public final static byte SUBTYPE_AVTP_CONTROL = (byte) 0x04;
+    public final static byte SUBTYPE_VENDOR = (byte) 0x6f;
+    public final static byte SUBTYPE_ADP = (byte) 0x7a;
+    public final static byte SUBTYPE_AECP = (byte) 0x7b;
+    public final static byte SUBTYPE_ACMP = (byte) 0x7c;
+    public final static byte SUBTYPE_MAAP = (byte) 0x7e;
+    public final static byte SUBTYPE_EXPERIMENTAL = (byte) 0x7f;
+
+    private static final Map<Integer, String> subtypeMap = createSubTypeMap();
+
+    public static final String getSubtypeName(byte subtype) {
+        if (subtypeMap.containsKey((int) subtype)) {
+            return subtypeMap.get((int) subtype);
+        } else {
+            return null;
+        }
+    }
 
     private static Map<Integer, String> createSubTypeMap() {
         Map<Integer, String> result = new HashMap<Integer, String>();
-        
-        result.put((int)SUBTYPE_61883_IIDC, "SUBTYPE_61883_IIDC");
-        result.put((int)SUBTYPE_MMA, "SUBTYPE_MMA");
-        result.put((int)SUBTYPE_AVTP_AUDIO, "SUBTYPE_AVTP_AUDIO");
-        result.put((int)SUBTYPE_AVTP_VIDEO, "SUBTYPE_AVTP_VIDEO");
-        result.put((int)SUBTYPE_AVTP_CONTROL, "SUBTYPE_AVTP_CONTROL");
-        result.put((int)SUBTYPE_VENDOR, "SUBTYPE_VENDOR");
-        result.put((int)SUBTYPE_ADP, "SUBTYPE_ADP");
-        result.put((int)SUBTYPE_AECP, "SUBTYPE_AECP");
-        result.put((int)SUBTYPE_ACMP, "SUBTYPE_ACMP");
-        result.put((int)SUBTYPE_MAAP, "SUBTYPE_MAAP");
-        result.put((int)SUBTYPE_EXPERIMENTAL, "SUBTYPE_EXPERIMENTAL");
+
+        result.put((int) SUBTYPE_61883_IIDC, "SUBTYPE_61883_IIDC");
+        result.put((int) SUBTYPE_MMA, "SUBTYPE_MMA");
+        result.put((int) SUBTYPE_AVTP_AUDIO, "SUBTYPE_AVTP_AUDIO");
+        result.put((int) SUBTYPE_AVTP_VIDEO, "SUBTYPE_AVTP_VIDEO");
+        result.put((int) SUBTYPE_AVTP_CONTROL, "SUBTYPE_AVTP_CONTROL");
+        result.put((int) SUBTYPE_VENDOR, "SUBTYPE_VENDOR");
+        result.put((int) SUBTYPE_ADP, "SUBTYPE_ADP");
+        result.put((int) SUBTYPE_AECP, "SUBTYPE_AECP");
+        result.put((int) SUBTYPE_ACMP, "SUBTYPE_ACMP");
+        result.put((int) SUBTYPE_MAAP, "SUBTYPE_MAAP");
+        result.put((int) SUBTYPE_EXPERIMENTAL, "SUBTYPE_EXPERIMENTAL");
         return Collections.unmodifiableMap(result);
     }
 
-	public final static byte getSubtype( ByteBuffer data ) {
-		byte v=(byte) (data.get(0) & subtype_mask);
-		return v;
-	}
-    
-	public byte getSubtype() {
-		return getSubtype(data);
-	}
+    public final static byte getSubtype(ByteBuffer data) {
+        byte v = (byte) (data.get(0) & subtype_mask);
+        return v;
+    }
 
-	public final static void setSubtype( ByteBuffer data, byte newValue) {
-		byte orig=(byte)(data.get(0) & (~subtype_mask) );
-		newValue &= subtype_mask;
-		newValue |= orig;
-		data.put(0, (byte) newValue );
-	}
-	
-	public void setSubtype(byte newValue) {
-		setSubtype( data, newValue );
-	}
+    public byte getSubtype() {
+        return getSubtype(data);
+    }
 
-	
+    public final static void setSubtype(ByteBuffer data, byte newValue) {
+        byte orig = (byte) (data.get(0) & (~subtype_mask));
+        newValue &= subtype_mask;
+        newValue |= orig;
+        data.put(0, (byte) newValue);
+    }
+
+    public void setSubtype(byte newValue) {
+        setSubtype(data, newValue);
+    }
+
+    public final static void setSV(ByteBuffer data, boolean newValue) {
+        setBit(data, 1, 0, newValue);
+    }
+
+    public void setSV(boolean newValue) {
+        setSV(data, newValue);
+    }
+
+    public final static boolean getSV(ByteBuffer data) {
+        return getBit(data, 1, 0);
+    }
+
+    public boolean getSV() {
+        return getSV(data);
+    }
+
+    public final static int version_mask = 0x7;
+    public final static int version_shift = (31 - 11);
+
+    public final static void setVersion(ByteBuffer data, int newValue) {
+        setBitField(data, 0, version_mask, version_shift, newValue);
+    }
+
+    public void setVersion(int newValue) {
+        setVersion(data, newValue);
+    }
+
+    public final static int getVersion(ByteBuffer data) {
+        return getBitField(data, 0, version_mask, version_shift);
+    }
+
+    public int getVersion() {
+        return getVersion(data);
+    }
+
+    public final static int type_specific_data_offset = 0;
+    public final static int type_specific_data_mask = 0xfffff;
+    public final static int type_specific_data_shift = (31 - 31);
+
+    public final static void setTypeSpecificData(ByteBuffer data, int newValue) {
+        setBitField(
+                data,
+                type_specific_data_offset,
+                type_specific_data_mask,
+                type_specific_data_shift,
+                newValue);
+    }
+
+    public void setTypeSpecificData(int newValue) {
+        setTypeSpecificData(data, newValue);
+    }
+
+    public final static int getTypeSpecificData(ByteBuffer data) {
+        return getBitField(
+                data,
+                type_specific_data_offset,
+                type_specific_data_mask,
+                type_specific_data_shift);
+    }
+
+    public int getTypeSpecificData() {
+        return getTypeSpecificData(data);
+    }
+
+    public final static int stream_id_offset = 4;
+
+    public final static void setStreamID(ByteBuffer data, byte[] eui64) {
+        setEUI64Field(data, stream_id_offset, eui64);
+    }
+
+    public void setStreamID( byte[] eui64 ) {
+        setStreamID(data,eui64);
+    }
+
+    public final static byte[] getStreamID(ByteBuffer data ) {
+        return getEUI64Field(data, stream_id_offset );
+    }
+
+    public byte[] getStreamID() {
+        return getStreamID( data );
+    }
+
 }
--- src/main/java/com/jdkoftinoff/jdksavdecc/PDU.java
+++ src/main/java/com/jdkoftinoff/jdksavdecc/PDU.java
@@ -4,226 +4,299 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 public class PDU {
-	public final static int MTU=1500;
-	private byte destination_address[];
-	private byte source_address[];
-	private short ethertype;
-	protected ByteBuffer data;
-	
-	PDU() {
-		this.destination_address = new byte[6];
-		this.source_address = new byte[6];
-		this.ethertype = (short)0x22f0;
-		this.data = ByteBuffer.allocateDirect(MTU);
-		this.data.order(ByteOrder.BIG_ENDIAN);		
-	}
 
-	PDU( PDU other ) {
-		this.destination_address = other.destination_address;
-		this.source_address = other.source_address;
-		this.ethertype = other.ethertype;
-		this.data = other.data;
-	}
-	
-	PDU( ByteBuffer data, byte destination_address[], byte source_address[], short ethertype ) {
-		this.destination_address = destination_address;
-		this.source_address = source_address;
-		this.ethertype = ethertype;
-		this.data = data;
-		this.data.order(ByteOrder.BIG_ENDIAN);
-	}
-	
-	public byte[] getDestinationAddress() {
-		return destination_address;
-	}
-	
-	public void setDestinationAddress( byte a[] ) {
-		destination_address = a;
-	}
+    public final static int MTU = 1500;
+    private byte destination_address[];
+    private byte source_address[];
+    private short ethertype;
+    protected ByteBuffer data;
 
-	
-	public byte[] getSourceAddress() {
-		return source_address;
-	}
-	
-	public void setSourceAddress( byte a[] ) {
-		source_address = a;
-	}
-	
-	public short getEthertype() {
-		return ethertype;
-	}
-	
-	public void setEthertype( short v ) {
-		ethertype = v;
-	}
-	
-	public ByteBuffer getData() {
-		return this.data;
-	}
-	
-	public void setData( ByteBuffer b ) {
-		data = b;
-	}
-	
-	public final static int getLength( ByteBuffer data) {
-		return data.capacity();
-	}
+    PDU() {
+        this.destination_address = new byte[6];
+        this.source_address = new byte[6];
+        this.ethertype = (short) 0x22f0;
+        this.data = ByteBuffer.allocateDirect(MTU);
+        this.data.order(ByteOrder.BIG_ENDIAN);
+    }
 
-	public int getLength() {
-		return this.data.capacity();
-	}
+    PDU(PDU other) {
+        this.destination_address = other.destination_address;
+        this.source_address = other.source_address;
+        this.ethertype = other.ethertype;
+        this.data = other.data;
+    }
 
-	public final static boolean getBit( ByteBuffer data, int start_offset, int bitnum_from_msb ) {
-		int additional = bitnum_from_msb/8;
-		int leftover = bitnum_from_msb&7;
-		byte mask = (byte)(0x80 >> leftover);
-		byte v = data.get(start_offset+additional);
-		
-		return (v&mask)!=0 ? true : false;
-	}
+    PDU(
+            ByteBuffer data,
+            byte destination_address[],
+            byte source_address[],
+            short ethertype) {
+        this.destination_address = destination_address;
+        this.source_address = source_address;
+        this.ethertype = ethertype;
+        this.data = data;
+        this.data.order(ByteOrder.BIG_ENDIAN);
+    }
 
-	public boolean getBit( int start_offset, int bitnum_from_msb ) {
-		return getBit(data,start_offset,bitnum_from_msb);
-	}
-	
-	public final static void setBit( ByteBuffer data, int start_offset, int bitnum_from_msb, boolean newValue ) {
-		int additional = bitnum_from_msb/8;
-		int leftover = bitnum_from_msb&7;
-		byte mask = (byte)(0x80 >> leftover);
-		byte orig = (byte) (data.get(start_offset+additional) & (~mask));
-		byte v = orig;
-		if( newValue ) {
-			v |= mask;
-		}
-		
-		data.put(start_offset+additional, v );
-	}
+    public byte[] getDestinationAddress() {
+        return destination_address;
+    }
 
-	public void setBit( int start_offset, int bitnum_from_msb, boolean newValue ) {
-		setBit( data, start_offset, bitnum_from_msb, newValue );
-	}
-	
-	public final static int getBitField( ByteBuffer data, int start_offset, int mask, int shift ) {
-		int v=(data.getInt(start_offset)>>shift) & mask;
-		return v;
-	}
+    public void setDestinationAddress(byte a[]) {
+        destination_address = a;
+    }
 
-	public int getBitField( int start_offset, int mask, int shift ) {
-		return getBitField(data,start_offset,mask,shift);
-	}
+    public byte[] getSourceAddress() {
+        return source_address;
+    }
 
-	public final static void setBitField( ByteBuffer data, int start_offset, int mask, int shift, int value ) {
-		int orig=data.getInt(start_offset) & (~ (mask<<shift) );
-		int v=((value&mask)<<shift);
-		data.putInt(start_offset, orig | v);
-	}
+    public void setSourceAddress(byte a[]) {
+        source_address = a;
+    }
 
-	public void setBitField( int start_offset, int mask, int shift, int value ) {
-		setBitField( data, start_offset, mask, shift, value );
-	}	
-	
-	public final static byte[] getEUI64Field( ByteBuffer data, int start_offset ) {
-		byte r[] = new byte[8];
-		data.get(r, start_offset, 8);
-		return r;
-	}
+    public short getEthertype() {
+        return ethertype;
+    }
 
-	public final static byte getOctetField( ByteBuffer data, int start_offset ) {
-		return data.get(start_offset);
-	}
+    public void setEthertype(short v) {
+        ethertype = v;
+    }
 
-	public byte getOctetField( int start_offset ) {
-		return getOctetField(data,start_offset);
-	}
+    public ByteBuffer getData() {
+        return this.data;
+    }
 
-	public final static void setOctetField( ByteBuffer data, int start_offset, byte newValue ) {
-		data.put(start_offset, newValue);
-	}
-	
-	public void setOctetField(  int start_offset, byte newValue ) {
-		setOctetField(data,start_offset,newValue);
-	}
-	
-	public final static short getDoubletField( ByteBuffer data, int start_offset ) {
-		return data.getShort(start_offset);
-	}
+    public void setData(ByteBuffer b) {
+        data = b;
+    }
 
-	public short getDoubletField( int start_offset ) {
-		return getDoubletField(data,start_offset);
-	}
+    public final static int getLength(
+            ByteBuffer data) {
+        return data.capacity();
+    }
 
-	public final static void setDoubletField( ByteBuffer data, int start_offset, short newValue ) {
-		data.putShort(start_offset, newValue);
-	}
-	
-	public void setDoubletField(  int start_offset, short newValue ) {
-		setDoubletField(data,start_offset,newValue);
-	}
+    public int getLength() {
+        return this.data.capacity();
+    }
 
-	
-	public final static int getQuadletField( ByteBuffer data, int start_offset ) {
-		return data.getInt(start_offset);
-	}
+    public final static boolean getBit(
+            ByteBuffer data,
+            int start_offset,
+            int bitnum_from_msb) {
+        int additional = bitnum_from_msb / 8;
+        int leftover = bitnum_from_msb & 7;
+        byte mask = (byte) (0x80 >> leftover);
+        byte v = data.get(start_offset + additional);
 
-	public int getQuadletField( int start_offset ) {
-		return getQuadletField(data,start_offset);
-	}
+        return (v & mask) != 0 ? true : false;
+    }
 
-	public final static void setQuadletField( ByteBuffer data, int start_offset, int newValue ) {
-		data.putInt(start_offset, newValue);
-	}
-	
-	public void setQuadletField(  int start_offset, int newValue ) {
-		setQuadletField(data,start_offset,newValue);
-	}
+    public boolean getBit(
+            int start_offset,
+            int bitnum_from_msb) {
+        return getBit(data, start_offset, bitnum_from_msb);
+    }
 
-	
-	public final static long getOctletField( ByteBuffer data, int start_offset ) {
-		return data.getLong(start_offset);
-	}
+    public final static void setBit(
+            ByteBuffer data,
+            int start_offset,
+            int bitnum_from_msb,
+            boolean newValue) {
+        int additional = bitnum_from_msb / 8;
+        int leftover = bitnum_from_msb & 7;
+        byte mask = (byte) (0x80 >> leftover);
+        byte orig = (byte) (data.get(start_offset + additional) & (~mask));
+        byte v = orig;
+        if (newValue) {
+            v |= mask;
+        }
 
-	public long getOctletField( int start_offset ) {
-		return getOctletField(data,start_offset);
-	}
+        data.put(start_offset + additional, v);
+    }
 
-	public final static void setOctletField( ByteBuffer data, int start_offset, long newValue ) {
-		data.putLong(start_offset, newValue);
-	}
-	
-	public void setOctletField(  int start_offset, long newValue ) {
-		setOctletField(data,start_offset,newValue);
-	}
-	
-	
-	public byte[] getEUI64Field( int start_offset ) {
-		return getEUI64Field( data, start_offset );
-	}
+    public void setBit(
+            int start_offset,
+            int bitnum_from_msb,
+            boolean newValue) {
+        setBit(data, start_offset, bitnum_from_msb, newValue);
+    }
 
-	public final static void setEUI64Field( ByteBuffer data, int start_offset, byte eui64[] ) {
-		data.put(eui64, start_offset, 8);
-	}
+    public final static int getBitField(
+            ByteBuffer data,
+            int start_offset,
+            int mask,
+            int shift) {
+        int v = (data.getInt(start_offset) >> shift) & mask;
+        return v;
+    }
 
-	public void setEUI64Field( int start_offset, byte eui64[] ) {
-		setEUI64Field( data, start_offset, eui64 );
-	}
-	
-	public final static byte[] getEUI48Field( ByteBuffer data, int start_offset ) {
-		byte r[] = new byte[6];
-		data.get(r, start_offset, 6);
-		return r;
-	}
+    public int getBitField(
+            int start_offset,
+            int mask,
+            int shift) {
+        return getBitField(data, start_offset, mask, shift);
+    }
 
-	public byte[] getEUI48Field( int start_offset ) {
-		return getEUI48Field( data, start_offset );
-	}
+    public final static void setBitField(
+            ByteBuffer data,
+            int start_offset,
+            int mask,
+            int shift,
+            int value) {
+        int orig = data.getInt(start_offset) & (~(mask << shift));
+        int v = ((value & mask) << shift);
+        data.putInt(start_offset, orig | v);
+    }
 
-	public final static void setEUI48Field( ByteBuffer data, int start_offset, byte eui48[] ) {
-		data.put(eui48, start_offset, 6);
-	}
+    public void setBitField(
+            int start_offset,
+            int mask,
+            int shift,
+            int value) {
+        setBitField(data, start_offset, mask, shift, value);
+    }
 
-	public void setEUI48Field( int start_offset, byte eui48[] ) {
-		setEUI48Field( data, start_offset, eui48 );
-	}
-		
+    public final static byte[] getEUI64Field(
+            ByteBuffer data,
+            int start_offset) {
+        byte r[] = new byte[8];
+        data.get(r, start_offset, 8);
+        return r;
+    }
+
+    public final static byte getOctetField(
+            ByteBuffer data,
+            int start_offset) {
+        return data.get(start_offset);
+    }
+
+    public byte getOctetField(int start_offset) {
+        return getOctetField(data, start_offset);
+    }
+
+    public final static void setOctetField(
+            ByteBuffer data,
+            int start_offset,
+            byte newValue) {
+        data.put(start_offset, newValue);
+    }
+
+    public void setOctetField(
+            int start_offset,
+            byte newValue) {
+        setOctetField(data, start_offset, newValue);
+    }
+
+    public final static short getDoubletField(
+            ByteBuffer data,
+            int start_offset) {
+        return data.getShort(start_offset);
+    }
+
+    public short getDoubletField(int start_offset) {
+        return getDoubletField(data, start_offset);
+    }
+
+    public final static void setDoubletField(
+            ByteBuffer data,
+            int start_offset,
+            short newValue) {
+        data.putShort(start_offset, newValue);
+    }
+
+    public void setDoubletField(
+            int start_offset,
+            short newValue) {
+        setDoubletField(data, start_offset, newValue);
+    }
+
+    public final static int getQuadletField(
+            ByteBuffer data,
+            int start_offset) {
+        return data.getInt(start_offset);
+    }
+
+    public int getQuadletField(int start_offset) {
+        return getQuadletField(data, start_offset);
+    }
+
+    public final static void setQuadletField(
+            ByteBuffer data,
+            int start_offset,
+            int newValue) {
+        data.putInt(start_offset, newValue);
+    }
+
+    public void setQuadletField(
+            int start_offset,
+            int newValue) {
+        setQuadletField(data, start_offset, newValue);
+    }
+
+    public final static long getOctletField(
+            ByteBuffer data,
+            int start_offset) {
+        return data.getLong(start_offset);
+    }
+
+    public long getOctletField(int start_offset) {
+        return getOctletField(data, start_offset);
+    }
+
+    public final static void setOctletField(
+            ByteBuffer data,
+            int start_offset,
+            long newValue) {
+        data.putLong(start_offset, newValue);
+    }
+
+    public void setOctletField(
+            int start_offset,
+            long newValue) {
+        setOctletField(data, start_offset, newValue);
+    }
+
+    public byte[] getEUI64Field(int start_offset) {
+        return getEUI64Field(data, start_offset);
+    }
+
+    public final static void setEUI64Field(
+            ByteBuffer data,
+            int start_offset,
+            byte eui64[]) {
+        data.put(eui64, start_offset, 8);
+    }
+
+    public void setEUI64Field(
+            int start_offset,
+            byte eui64[]) {
+        setEUI64Field(data, start_offset, eui64);
+    }
+
+    public final static byte[] getEUI48Field(
+            ByteBuffer data,
+            int start_offset) {
+        byte r[] = new byte[6];
+        data.get(r, start_offset, 6);
+        return r;
+    }
+
+    public byte[] getEUI48Field(
+            int start_offset) {
+        return getEUI48Field(data, start_offset);
+    }
+
+    public final static void setEUI48Field(
+            ByteBuffer data,
+            int start_offset,
+            byte eui48[]) {
+        data.put(eui48, start_offset, 6);
+    }
+
+    public void setEUI48Field(
+            int start_offset,
+            byte eui48[]) {
+        setEUI48Field(data, start_offset, eui48);
+    }
+
 }
